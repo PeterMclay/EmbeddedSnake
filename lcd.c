8b2da73f35a7ac8ab73048bb8e227b04171f0635
PeterMclay <36018782+PeterMclay@users.noreply.github.com>
2019-03-27 21:59:12 -0400

    Added Rand Food Placement, Eating, Snake with large length, and square girth

--- lcd.c
+++ lcd.c
@@ -1,15 +1,17 @@
 #include "lcd_driver.h"
 #include "lcd_graphic.h"
 #include "address_map_arm.h"
+#include <stdlib.h>
 /*******************************************************************************
-							        SNAKE
- ******************************************************************************/
- volatile int* BTN = (int*)KEY_BASE;
- int crntBTN;
- int prevBTN = 0;
+SNAKE
+******************************************************************************/
+volatile int* BTN = (int*)KEY_BASE;
+int crntBTN;
+int prevBTN = 0;
 
 int main(void) {
-	while(1){
+	char snke[10] = "SNAKE\0";
+	while (1) {
 		//int x, y, 
 		int pos_x_current[3] = { 64, 56, 48 };
 		int pos_y_current[3] = { 24, 24, 24 };
@@ -20,24 +22,28 @@ int main(void) {
 		int i;
 		volatile int delay_count; // volatile so C compiler doesn't remove the loop
 
-		//Initialize LCD
+								  //Initialize LCD
 		init_spim0();
 		init_lcd();
 		clear_screen();
-		
+
 		//Press Button to generate head
-		char snke[10] = "SNAKE\0";
+		//char snke[10] = "SNAKE\0";
 		LCD_text(snke, 0);
 		refresh_buffer();
-		while(ReadButtons() != 1);
+		while (ReadButtons() == 0);
 		clear_screen();
 		refresh_buffer();
 		crntBTN = 0;
 
 		//Initialize Snake
-		length = 8;
-		dir_x  = 0;
-		dir_y  = 0;
+		length = 4;
+		dir_x = 0;
+		dir_y = 0;
+		int rx = rand() % 124 + 1;
+		int ry = rand() % 65 + 1;
+		rx = rx - rx % 4;
+		ry = ry - ry % 4;
 		for (i = 0; i < size; i++) {
 			LCD_rect(pos_x_current[i], pos_y_current[i], length, length, 1, 1);
 		}
@@ -50,8 +56,33 @@ int main(void) {
 				LCD_rect(pos_x_current[i], pos_y_current[i], length, length, 0, 1);
 			}
 
+			// Update Snake Direction
+			if (ReadButtons()) {
+				if (crntBTN != prevBTN) {
+					if (crntBTN == 1 && dir_x != -4) {
+						dir_x = 4;
+						dir_y = 0;
+					}
+					if (crntBTN == 2 && dir_x != 4) {
+						dir_x = -4;
+						dir_y = 0;
+					}
+					if (crntBTN == 4 && dir_y != 4) {
+						dir_y = -4;
+						dir_x = 0;
+					}
+					if (crntBTN == 8 && dir_y != -4) {
+						dir_y = 4;
+						dir_x = 0;
+					}
+					prevBTN = crntBTN;
+				}
+				else
+					prevBTN = crntBTN;
+			}
+
 			//Check if Snake is out of Bounds in x and y
-			if ((pos_x_current[0] + length >= SCREEN_WIDTH - 1 && dir_x == 1) || (pos_x_current[0] <= 0 && dir_x == -1)) {
+			if ((pos_x_current[0] + length >= SCREEN_WIDTH + -4 && dir_x == 4) || (pos_x_current[0] <= 0 && dir_x == -4)) {
 				dir_x = 0;
 				dir_y = 0;
 				char text_gameover_lcd[17] = "Game over :(\0";
@@ -59,11 +90,13 @@ int main(void) {
 				char text_top_lcd[35] = "Press Any ButtonTo Play Again\0";
 				LCD_text(text_top_lcd, 1);
 				refresh_buffer();
-				while(ReadButtons() == 0);
+				while (ReadButtons() == 0);
 				crntBTN = 0;
+				prevBTN = 6;
+				for (delay_count = 950000; delay_count != 0; --delay_count);
 				break;
 			}
-			if ((pos_y_current[0] + length >= SCREEN_HEIGHT - 1 && dir_y == 1) ||(pos_y_current[0] <= 0 && dir_y == -1)){
+			if ((pos_y_current[0] + length >= SCREEN_HEIGHT - 1 && dir_y == 4) || (pos_y_current[0] <= 0 && dir_y == -4)) {
 				dir_x = 0;
 				dir_y = 0;
 				char text_gameover_lcd[17] = "Game over :( \0";
@@ -72,34 +105,11 @@ int main(void) {
 				char text_top_lcd[35] = "Press Any ButtonTo Play Again\0";
 				LCD_text(text_top_lcd, 1);
 				refresh_buffer();
-				while(ReadButtons() == 0);
+				while (ReadButtons() == 0);
 				crntBTN = 0;
+				prevBTN = 6;
+				for (delay_count = 950000; delay_count != 0; --delay_count);
 				break;
-			}
-			
-			// Update Snake Direction
-			if(ReadButtons()){
-				if (crntBTN != prevBTN) {
-					if (crntBTN == 1 && dir_x != -1) {
-						dir_x = 8;
-						dir_y = 0;
-					}
-					if (crntBTN == 2 && dir_x != 1) {
-						dir_x = -8;
-						dir_y = 0;
-					}
-					if (crntBTN == 4 && dir_y != 1) {
-						dir_y = -8;
-						dir_x = 0;
-					}
-					if (crntBTN == 8 && dir_y != -1) {
-						dir_y = 8;
-						dir_x = 0;
-					}
-					prevBTN = crntBTN;
-				}
-				else
-					prevBTN = crntBTN;
 			}
 
 			//Move Sanke
@@ -116,36 +126,39 @@ int main(void) {
 				pos_x_previous[i] = pos_x_current[i];
 				pos_y_previous[i] = pos_y_current[i];
 			}
+			LCD_rect(rx, ry, length, length, 1, 1);
+			if ((pos_x_current[0] == rx) && (pos_y_current[0] == ry)) {
+				rx = rand() % 124 + 1;
+				ry = rand() % 65 + 1;
+
+				rx = rx - rx % 4;
+				ry = ry - ry % 4;
+			}
 			refresh_buffer();
-			
 			// Delay Loop
-			for (delay_count = 100000; delay_count != 0; --delay_count); 
+			for (delay_count = 950000; delay_count != 0; --delay_count);
 		}
 	}
 }
 
-int ReadButtons(void){
-	if ((((*BTN)&0x01) == 1)){
+int ReadButtons(void) {
+	if ((((*BTN) & 0x01) == 1)) {
 		// while((((*BTN)&0x01) == 1));
-		printf("right");
 		crntBTN = 1;
 		return 1;
 	}
 	if ((((*BTN) & 0x02) == 2)) {
 		// while ((((*BTN) & 0x02) == 2));
-		printf("left");
 		crntBTN = 2;
 		return 1;
 	}
 	if ((((*BTN) & 0x04) == 4)) {
 		// while ((((*BTN) & 0x04) == 4));
-		printf("up");
 		crntBTN = 4;
 		return 1;
 	}
 	if ((((*BTN) & 0x08) == 8)) {
 		// while ((((*BTN) & 0x08) == 8));
-		printf("down");
 		crntBTN = 8;
 		return 1;
 	}
