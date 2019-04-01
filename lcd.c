6d96c54d00b4cd9dd17bdda95b3bd223163c5f36
PeterMclay <36018782+PeterMclay@users.noreply.github.com>
2019-04-01 17:58:21 -0400

    Cleaned and Refined Code, Food Spawn, Collision, and Added Hex Display

--- lcd.c
+++ lcd.c
@@ -5,52 +5,119 @@
 /*******************************************************************************
 SNAKE
 ******************************************************************************/
+
+//Global Variables
 volatile int* BTN = (int*)KEY_BASE;
 int crntBTN;
 int prevBTN = 0;
+volatile int *HEX3_HEX0_BASE_ptr = (int*)HEX3_HEX0_BASE;
+unsigned char lookUpTable[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67 };
 
+//Functions
+void display_score(int sze) {
+	*(HEX3_HEX0_BASE_ptr) = (256 * lookUpTable[(sze - 1) / 10] + lookUpTable[(sze - 1) % 10]);
+}
+int ReadButtons(void) {
+	if ((((*BTN) & 0x01) == 1)) {
+		// while((((*BTN)&0x01) == 1));
+		crntBTN = 1;
+		return 1;
+	}
+	if ((((*BTN) & 0x02) == 2)) {
+		// while ((((*BTN) & 0x02) == 2));
+		crntBTN = 2;
+		return 1;
+	}
+	if ((((*BTN) & 0x04) == 4)) {
+		// while ((((*BTN) & 0x04) == 4));
+		crntBTN = 4;
+		return 1;
+	}
+	if ((((*BTN) & 0x08) == 8)) {
+		// while ((((*BTN) & 0x08) == 8));
+		crntBTN = 8;
+		return 1;
+	}
+	return 0;
+}
+int game_over(int *posx, int *posy, int dir_x, int dir_y, int length, int size) {
+	if ((posx[0] + length >= SCREEN_WIDTH + -4 && dir_x == 4) || (posx[0] <= 0 && dir_x == -4)) {
+		return 1;
+	}
+	if ((posy[0] + length >= SCREEN_HEIGHT - 4 && dir_y == 4) || (posy[0] <= 0 && dir_y == -4)) {
+		return 1;
+	}
+	for (int i = 1; i < size; i++) {
+		if (posx[0] == posx[i] && posy[0] == posy[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+//Main
 int main(void) {
+
+	//Initialize LCD
+	init_spim0();
+	init_lcd();
+	clear_screen();
+
+	//Variable Declaration
 	char snke[10] = "SNAKE\0";
+	char text_gameover_upper[17] = "Game over\0";
+	char text_gameover_lower[35] = "Press a Button to Play Again\0";
+	int pos_x_current[25];
+	int pos_y_current[25];
+	int pos_x_previous[25];
+	int pos_y_previous[25];
+	int dir_x = 0;
+	int dir_y = 0;
+	int size;
+	int length = 4;
+	int i;
+	volatile int delay_count; // volatile so C compiler doesn't remove the loop
+
 
 	while (1) {
-		//int x, y, 
-		int pos_x_current[25] = { 64 };
-		int pos_y_current[25] = { 24 };
-		int pos_x_previous[25] = { 64 };
-		int pos_y_previous[25] = { 24 };
-		int length, dir_x, dir_y;
-		int size = 1;
-		int i;
-		volatile int delay_count; // volatile so C compiler doesn't remove the loop
-		int collision = 0;
+		//Starting Position of the Snake
+		pos_x_current[0] = 64;
+		pos_y_current[0] = 24;
+		pos_x_previous[0] = 64;
+		pos_y_previous[0] = 24;
+		dir_x = 0;
+		dir_y = 0;
+		size = 1;
 
-
-		//Initialize LCD
-		init_spim0();
-		init_lcd();
-		clear_screen();
-
-		//Press Button to generate head
-		//char snke[10] = "SNAKE\0";
-
-		//LCD_text(snke, 0);
+		//Display Snake at Top of Screen
+		LCD_text(snke, 0);
+		display_score(size);
 		refresh_buffer();
 		while (ReadButtons() == 0);
 		clear_screen();
-		//refresh_buffer();
+		refresh_buffer();
 		crntBTN = 0;
 
-		//Initialize Snake
-		length = 4;
-		dir_x = 0;
-		dir_y = 0;
+		//Initialize Snake Food
 		int rx = rand() % 124 + 1;
-		int ry = rand() % 65 + 1;
+		int ry = rand() % 63 + 1;
 		rx = rx - rx % 4;
 		ry = ry - ry % 4;
+
+		//Ensure Food is not on Snake
 		for (i = 0; i < size; i++) {
-			LCD_rect(pos_x_current[i], pos_y_current[i], length, length, 1, 1);
+			if ((rx == pos_x_current[i] && ry == pos_y_current[i]) && (size == 1)) {
+				rx = rand() % 124 + 1;
+				ry = rand() % 63 + 1;
+				rx = rx - rx % 4;
+				ry = ry - ry % 4;
+				i--;
+			}
 		}
+
+		//Draw Snake, Food, and Arena
+		LCD_rect(pos_x_current[i], pos_y_current[i], length, length, 1, 1);
+		LCD_rect(rx, ry, length, length, 1, 1);
 		LCD_line(0, 0, 61, 1, 1);
 		LCD_line(127, 0, 61, 1, 1);
 		LCD_line(0, 61, 127, 1, 0);
@@ -59,16 +126,21 @@ int main(void) {
 
 		/***********	GAME ON **********/
 		while (1) {
-			/* erase box */
+
+			//Draw Border
+			LCD_line(0, 0, 61, 1, 1);
+			LCD_line(127, 0, 61, 1, 1);
+			LCD_line(0, 61, 127, 1, 0);
+			LCD_line(5, 0, 60, 1, 0);
+			// Draw Food Location
+			LCD_rect(rx, ry, length, length, 1, 1);
+
+			//Erase Snake
 			for (i = 0; i < size; i++) {
 				LCD_rect(pos_x_current[i], pos_y_current[i], length, length, 0, 1);
 			}
 
-
-			//LCD_line(0, 60, 48, 1, 0);			
-
-
-			// Update Snake Direction
+			//Snake Direction
 			if (ReadButtons()) {
 				if (crntBTN != prevBTN) {
 					if (crntBTN == 1 && dir_x != -4) {
@@ -93,61 +165,6 @@ int main(void) {
 					prevBTN = crntBTN;
 			}
 
-			//Check if Snake is out of Bounds in x and y
-			if ((pos_x_current[0] + length >= SCREEN_WIDTH + -4 && dir_x == 4) || (pos_x_current[0] <= 0 && dir_x == -4)) {
-				dir_x = 0;
-				dir_y = 0;
-				char text_gameover_lcd[17] = "Game over :(\0";
-				LCD_text(text_gameover_lcd, 0);
-				char text_top_lcd[35] = "Press Any ButtonTo Play Again\0";
-				LCD_text(text_top_lcd, 1);
-				refresh_buffer();
-				while (ReadButtons() == 0);
-				crntBTN = 0;
-				prevBTN = 6;
-				for (delay_count = 950000; delay_count != 0; --delay_count);
-				break;
-			}
-			if ((pos_y_current[0] + length >= SCREEN_HEIGHT - 4 && dir_y == 4) || (pos_y_current[0] <= 0 && dir_y == -4)) {
-				dir_x = 0;
-				dir_y = 0;
-				char text_gameover_lcd[17] = "Game over :( \0";
-				LCD_text(text_gameover_lcd, 0);
-				refresh_buffer();
-				char text_top_lcd[35] = "Press Any ButtonTo Play Again\0";
-				LCD_text(text_top_lcd, 1);
-				refresh_buffer();
-				while (ReadButtons() == 0);
-				crntBTN = 0;
-				prevBTN = 6;
-				for (delay_count = 950000; delay_count != 0; --delay_count);
-				break;
-			}
-
-			for (i = 1; i <size; i++) {
-				if (pos_x_current[0] == pos_x_current[i] && pos_y_current[0] == pos_y_current[i]) {
-					++collision;
-				}
-			}
-
-			if (collision != 0) {
-				dir_x = 0;
-				dir_y = 0;
-				char text_gameover_lcd[17] = "Game over :( \0";
-				LCD_text(text_gameover_lcd, 0);
-				refresh_buffer();
-				char text_top_lcd[35] = "self coll\0";
-				LCD_text(text_top_lcd, 1);
-				refresh_buffer();
-				while (ReadButtons() == 0);
-				crntBTN = 0;
-				collision = 1;
-				prevBTN = 6;
-				for (delay_count = 950000; delay_count != 0; --delay_count);
-				collision = 0;
-				break;
-			}
-
 			//Move Sanke
 			pos_x_current[0] += dir_x;
 			pos_y_current[0] += dir_y;
@@ -156,28 +173,28 @@ int main(void) {
 				pos_y_current[i] = pos_y_previous[i - 1];
 			}
 
-			//Food Location
-			LCD_rect(rx, ry, length, length, 1, 1);
-
 			//Eats Food
 			if ((pos_x_current[0] == rx) && (pos_y_current[0] == ry)) {
-
 				rx = rand() % 124 + 1;
 				ry = rand() % 64 + 1;
 				rx = rx - rx % 4;
 				ry = ry - ry % 4;
-
 				for (i = 0; i<size; i++) {
-					if (rx == pos_x_current[i] || ry == pos_y_current[i]) {
-						i = 0;
+					if ((rx == pos_x_current[i] && ry == pos_y_current[i]) && (size == 1)) {
 						rx = rand() % 124 + 1;
 						ry = rand() % 63 + 1;
 						rx = rx - rx % 4;
 						ry = ry - ry % 4;
+						i--;
 					}
-
+					if ((rx == pos_x_current[i] && ry == pos_y_current[i]) && (size != 1)) {
+						rx = rand() % 124 + 1;
+						ry = rand() % 63 + 1;
+						rx = rx - rx % 4;
+						ry = ry - ry % 4;
+						i = 0;
+					}
 				}
-
 				if (dir_x == 4) {
 					pos_x_current[size] = pos_x_current[size - 1] - 4;
 					pos_y_current[size] = pos_y_current[size - 1];
@@ -195,6 +212,7 @@ int main(void) {
 					pos_x_current[size] = pos_x_current[size - 1];
 				}
 				++size;
+				display_score(size);
 			}
 
 			// Update Position
@@ -204,33 +222,24 @@ int main(void) {
 				pos_y_previous[i] = pos_y_current[i];
 			}
 
+			//Check Collision and Gameover
+			if (game_over(pos_x_current, pos_y_current, dir_x, dir_y, length, size) == 1) {
+				clear_screen();
+				refresh_buffer();
+				LCD_text(text_gameover_upper, 0);
+				LCD_text(text_gameover_lower, 1);
+				refresh_buffer();
+				while (ReadButtons() == 0);
+				clear_screen();
+				refresh_buffer();
+				prevBTN = -1;
+				for (delay_count = 1000000; delay_count != 0; --delay_count);
+				break;
+			}
+
 			// Update Screen & Delay Loop
 			refresh_buffer();
 			for (delay_count = 950000; delay_count != 0; --delay_count);
 		}
 	}
-}
-
-int ReadButtons(void) {
-	if ((((*BTN) & 0x01) == 1)) {
-		// while((((*BTN)&0x01) == 1));
-		crntBTN = 1;
-		return 1;
-	}
-	if ((((*BTN) & 0x02) == 2)) {
-		// while ((((*BTN) & 0x02) == 2));
-		crntBTN = 2;
-		return 1;
-	}
-	if ((((*BTN) & 0x04) == 4)) {
-		// while ((((*BTN) & 0x04) == 4));
-		crntBTN = 4;
-		return 1;
-	}
-	if ((((*BTN) & 0x08) == 8)) {
-		// while ((((*BTN) & 0x08) == 8));
-		crntBTN = 8;
-		return 1;
-	}
-	return 0;
 }
